f6bb3fd487b92ef9628cb24ee95e669500b5029e
SESHAM SURYA VAMSI <suryavamsis005@gmail.com>
2025-08-14 14:35:01 +0530

    test commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div>
+        <h1>Software Engineer and <span>Data Base Handler</span></h1>
+        <p>Surya Welcomes You to know about be Go to my Profile.</p>
+      </div>
+    </div>
+  );
+}
