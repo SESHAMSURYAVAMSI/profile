5d0b14019ec59c4f4ed6cf8c30e5b1355191d782
SESHAMSURYAVAMSI <suryavamsis005@gmail.com>
2025-09-07 12:28:37 +0530

    changes done

--- my-profile/src/app/layout.tsx
+++ my-profile/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import BubbleCursor from "@/components/ui/BubbleCursor";
   
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <BubbleCursor />
+        
       </body>
     </html>
   );
